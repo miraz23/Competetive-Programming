58513cd42979805f0555f10fe2977af7041259fd
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-05-02 23:26:45 +0600

    943 Div-3

---
+++ B_Prefiquence.cpp
@@ -0,0 +1,54 @@
+/*
+
+||-------------------------------||
+||      Mohiul Islam Miraz       ||
+||-------------------------------||
+
+*/
+
+#include <bits/stdc++.h>
+#define swift                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define sac sort(v.begin(), v.end())
+#define sdc sort(v.begin(), v.end(), greater<int>())
+#define f1 bool flag = true
+#define f0 bool flag = false
+#define c0 int count = 0
+#define c1 int count = 1
+#define s0 long long sum = 0
+#define a0 long long ans = 0
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cnt cout << count << endl
+#define sm cout << sum << endl
+using namespace std;
+
+int main()
+{
+    swift;
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        string s1, s2;
+        cin >> s1 >> s2;
+        c0;
+
+        for (int i = 0, j = 0; i < s1.size(), j < s2.size(); i++, j++)
+        {
+
+            if (s1[i] == s2[j]) count++;
+            else --i;
+        }
+        cnt;
+    }
+
+    return 0;
+}
