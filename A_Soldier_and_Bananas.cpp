c86d4178f5e351ed8ef11d94a26ba9173b471ed4
Md.Mohiul Islam Miraz <mohiulislam23@gmail.com>
2024-05-03 23:11:45 +0600

    800

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,48 @@
+/*
+
+||-------------------------------||         
+||      Mohiul Islam Miraz       ||
+||-------------------------------||
+
+*/
+
+#include <bits/stdc++.h>
+#define swift                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define sac sort(v.begin(), v.end())
+#define sdc sort(v.begin(), v.end(), greater<int>())
+#define f1 bool flag = true
+#define f0 bool flag = false
+#define c0 int count = 0
+#define c1 int count = 1
+#define s0 long long sum = 0
+#define a0 long long ans = 0
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cnt cout << count << endl
+#define sm cout << sum << endl
+using namespace std;
+
+int main()
+{
+    swift;
+    int t = 1;
+    //cin >> t;
+
+    while (t--)
+    {
+        int k, n, w; cin >> k >> n >> w;
+        s0;
+        for(int i = 1; i <= w; i++)
+        {
+            sum += k*i;
+        }
+        if(sum < n) cout << 0 << endl;
+        else cout << sum - n << endl;
+    }
+
+    return 0;
+}
