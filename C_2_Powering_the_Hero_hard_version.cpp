aa68991d64b886a3b8e01a4adf98178775c506f4
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-02-01 00:00:16 +0600

    1100

---
+++ C_2_Powering_the_Hero_hard_version.cpp
@@ -0,0 +1,62 @@
+/*
+
+||-------------------------------||         
+||      Mohiul Islam Miraz       ||
+||-------------------------------||
+
+*/
+
+#include <bits/stdc++.h>
+#define swift                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define sac sort(v.begin(), v.end())
+#define sdc sort(v.begin(), v.end(), greater<int>())
+#define f1 bool flag = true
+#define f0 bool flag = false
+#define c0 int count = 0
+#define c1 int count = 1
+#define s0 long long sum = 0
+#define a0 long long ans = 0
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cnt cout << count << endl
+#define sm cout << sum << endl
+using namespace std;
+
+int main()
+{
+    swift;
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        int n; cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        priority_queue<int> pq;
+        s0;
+        for(int i = 0; i < n; i++)
+        {
+            if(v[i] == 0)
+            {
+                if(!pq.empty())
+                {
+                    sum+=pq.top();
+                    pq.pop();
+                }
+            }
+            else pq.push(v[i]);
+        }
+        sm;
+        
+    }
+
+    return 0;
+}
