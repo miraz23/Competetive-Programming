161ab8f751a9d875ce1e243056cf2f2617680a0d
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-02-25 21:47:43 +0600

    1100

---
+++ A_Parallelepiped.cpp
@@ -0,0 +1,48 @@
+// Link: https://codeforces.com/problemset/problem/224/A
+
+/*
+
+||-------------------------------||         
+||      Mohiul Islam Miraz       ||
+||-------------------------------||
+
+*/
+
+#include <bits/stdc++.h>
+#define swift                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define sac sort(v.begin(), v.end())
+#define sdc sort(v.begin(), v.end(), greater<int>())
+#define f1 bool flag = true
+#define f0 bool flag = false
+#define c0 int count = 0
+#define c1 int count = 1
+#define s0 long long sum = 0
+#define a0 long long ans = 0
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cnt cout << count << endl
+#define sm cout << sum << endl
+using namespace std;
+
+int main()
+{
+    swift;
+    int t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+        int ab, bc, ca; cin >> ab >> bc >> ca;
+        int a = sqrt((ab*ca)/bc);
+        int b = sqrt((ab*bc)/ca);
+        int c = sqrt((ca*bc)/ab);
+
+        cout << 4 * (a + b + c) << endl;
+    }
+
+    return 0;
+}
