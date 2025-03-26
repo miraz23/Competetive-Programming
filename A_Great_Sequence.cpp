37c3fdc63bed6d29d3e4afc45faa88422d4dbd7c
Md. Mohiul Islam Miraz <mohiulislam23@gmail.com>
2025-03-26 17:45:00 +0600

    1200 TLE

---
+++ A_Great_Sequence.cpp
@@ -0,0 +1,67 @@
+/*
+
+||-------------------------------||
+||      Mohiul Islam Miraz       ||
+||-------------------------------||
+
+*/
+
+#include <bits/stdc++.h>
+#define swift                     \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
+#define sac sort(v.begin(), v.end())
+#define sdc sort(v.begin(), v.end(), greater<int>())
+#define f1 bool flag = true
+#define f0 bool flag = false
+#define c0 ll count = 0
+#define c1 int count = 1
+#define s0 long long sum = 0
+#define a0 long long ans = 0
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define minus cout << -1 << endl
+#define cnt cout << count << endl
+#define sm cout << sum << endl
+using namespace std;
+
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    
+    vector<int> v(n);
+    unordered_map<int, int> freq;
+    
+    for (int &num : v) {
+        cin >> num;
+        freq[num]++;
+    }
+    sac; c0;
+    for (int num : v) {
+        if (freq[num] == 0) continue;
+        
+        if (freq[num * x] > 0) {
+            freq[num]--;
+            freq[num * x]--;
+        } else {
+            freq[num]--;
+            count++;
+        }
+    }
+    cnt;
+}
+
+int main()
+{
+    swift;
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
